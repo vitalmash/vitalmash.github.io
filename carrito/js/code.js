29161b52d030761f38a5bbe4724573fe102188d0
vitalmash <117310901+vitalmash@users.noreply.github.com>
2022-11-18 14:52:57 -0500

    Add files via upload

--- carrito/js/code.js
+++ carrito/js/code.js
@@ -1,10 +1,21 @@
-const PW = document.querySelector(".product-view");
 const PW__main = document.querySelector(".product-view__main");
 const PW__filing = document.querySelector(".product-view__filing");
+const FP__thumbnails = document.querySelector(".filing-product__thumbnails");
+const FP__picture = document.querySelector(".filing-product__picture");
 const PW__info = document.querySelector(".product-view__info");
 const IP__title = document.querySelector(".info-product__title");
 const IP__score = document.querySelector(".info-product__score");
 const IP__cost = document.querySelector(".info-product__cost");
+
+if (window.location.pathname == '/carrito/index.html') {
+    FP__picture.addEventListener('click', ()=>{
+        if (FP__thumbnails.classList.contains("moveToDown") && window.innerWidth <= 1024) {
+            FP__thumbnails.classList.replace("moveToDown", "moveToUp");
+        } else if (window.innerWidth <= 1024) {
+            FP__thumbnails.classList.add("moveToDown");     
+        }
+    });
+}
 
 export function changeOnresizeOnload() {
     changeOnProductView__filing();
@@ -19,3 +30,4 @@ function changeOnProductView__filing() {
         PW__info.insertBefore(IP__score, IP__cost);
     }
 }
+
